7dd93c947cfed5fd5c1e0e9b82e076f520cdc875
Your Name <stoner.michann@gmail.com>
2021-06-08 17:40:52 -0600

    Set up class component structure

---
+++ src/components/Form/Form.js
@@ -0,0 +1,19 @@
+import './Form.css'
+import { Component } from 'react'
+
+class Form extends Component {
+  constructor() {
+    super()
+    this.state = {
+      searchBarValue: ''
+    }
+  }
+
+  render() {
+    return (
+      <form>
+        
+      </form>
+    )
+  }
+}
