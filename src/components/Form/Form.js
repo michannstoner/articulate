f98dc407014372ab24c2719172eca6b874c98281
Your Name <stoner.michann@gmail.com>
2021-06-08 17:52:58 -0600

    Write onChange method for search input

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -9,14 +9,21 @@ class Form extends Component {
     }
   }
 
+  handleChange = event => {
+    this.setState({ searchBarValue: event.target.value })
+  }
+
   render() {
+    console.log(this.state.searchBarValue)
     return (
       <form className='search-bar'>
         <input 
           type='text'
           placeholder='search for a word'
           value={this.state.searchBarValue}
+          onChange={event => this.handleChange(event)}
         />
+        <button>search</button>
       </form>
     )
   }
