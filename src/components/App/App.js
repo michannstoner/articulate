f33b3309b7321c48bfeeb77078c6280fd3a0a4d3
Your Name <stoner.michann@gmail.com>
2021-06-09 11:07:59 -0600

    Fix spacing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -39,7 +39,7 @@ class App extends Component {
   }
 
   render() {
-    console.log(this.state.favorites);
+  
     return (
       <main className='main'>
         <Nav />
@@ -47,21 +47,19 @@ class App extends Component {
           render={() => (
             <div>
               <Form submitSearch={this.submitSearch}/>
-              {!this.state.wordToDisplay && <section className='welcome-display'>
-              <h2>welcome to ARTICULATE.</h2>
-              <p>search for a word to learn more</p>
-              </section>}
-              {this.state.wordToDisplay &&<WordInfo 
-              wordToDisplay={this.state.wordToDisplay}
-              addToFavorites={this.addToFavorites}
-          />}
-
+              {!this.state.wordToDisplay && 
+                <section className='welcome-display'>
+                  <h2>welcome to ARTICULATE.</h2>
+                  <p>search for a word to learn more</p>
+                </section>}
+              {this.state.wordToDisplay &&
+                <WordInfo 
+                  wordToDisplay={this.state.wordToDisplay}
+                  addToFavorites={this.addToFavorites}
+                />}
             </div>
-
           )}
         />
-        
-         
         <Route exact path='/favorites'
           render={() => (
             !this.state.favorites.length ?
