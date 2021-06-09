7d905c0243bf0bc31639a913ffcf6da26955cef7
Your Name <stoner.michann@gmail.com>
2021-06-09 12:00:28 -0600

    Remove console.log

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -2,7 +2,6 @@ import './Favorites.css'
 import Word from '../Word/Word'
 
 const Favorites = ({ favoriteWords }) => {
-  console.log(favoriteWords);
   const wordsToDisplay = favoriteWords.map(word => {
     return <Word
               key={word.frequency}
