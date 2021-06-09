46e207c56e9937e18d34162ee5c32ace44f64910
Your Name <stoner.michann@gmail.com>
2021-06-09 13:56:45 -0600

    Add return in function

--- src/utils/api-calls.js
+++ src/utils/api-calls.js
@@ -7,14 +7,14 @@ const checkForError = response => {
 }
 
 const fetchWord = word => {
-  fetch(`https://wordsapiv1.p.rapidapi.com/words/${word}`, {
+  return fetch(`https://wordsapiv1.p.rapidapi.com/words/${word}`, {
 	      "method": "GET",
 	      "headers": {
 		      "x-rapidapi-key": "247406a57bmsh73bf0cce7ec0b4cp15cc72jsnf9d8d4aae1f1",
 		      "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
 	      }
     })
-    .then(checkResponse)
+    .then(checkForError)
 }
 
 export { fetchWord, checkForError }
