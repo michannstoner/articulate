1253c816572d3b9e2e0576b13bb497973d925722
Your Name <stoner.michann@gmail.com>
2021-06-09 12:00:17 -0600

    Change styling for links

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,11 +2,14 @@ import './Nav.css'
 import { Link } from 'react-router-dom'
 
 const Nav = () => {
+  const linkStyle = {textDecoration: 'none', color: 'black'}
 
   return (
     <nav className='nav'>
+    <Link to='/' style={linkStyle}>
       <h1>A.</h1>
-    <Link to={'/favorites'}>
+    </Link>
+    <Link to={'/favorites'} style={linkStyle}>
       <ul>favorites</ul>
     </Link>
     </nav>
