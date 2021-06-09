1f9486cff8630950a4f62902e1ff927d53a74135
Your Name <stoner.michann@gmail.com>
2021-06-09 11:06:01 -0600

    Add link to nav favorites

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,14 @@
 import './Nav.css'
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
 
   return (
     <nav className='nav'>
       <h1>A.</h1>
+    <Link to={'/favorites'}>
       <ul>favorites</ul>
+    </Link>
     </nav>
   )
 }
