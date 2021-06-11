867a8ca629a4197ba212e76209f2947ae00c25d8
Your Name <stoner.michann@gmail.com>
2021-06-11 09:40:46 -0600

    Move array formatting

--- src/components/Word/Word.js
+++ src/components/Word/Word.js
@@ -3,16 +3,15 @@ import PropTypes from 'prop-types'
 import pinkSquare from '../../assets/pink-square-icon.png'
 
 const Word = ({ word, pronunciation, definition, synonyms }) => {
-  const formattedSynonyms = synonyms.join(', ')
   return (
     <article className='word-card'>
       <h3 className='single-word'>{word}</h3>
       <div className='definition-divider'>
-        <img className='pink-square' src={pinkSquare}/>
+        <img className='pink-square' src={pinkSquare} alt='decorative pink square icon'/>
         <p className='definition'>{definition}</p>
       </div>
       <p><strong>pronunciation:</strong> {pronunciation}</p>
-      <p><strong>synonyms:</strong> {formattedSynonyms}</p>
+      <p><strong>synonyms:</strong> {synonyms}</p>
     </article>
   )
 }
