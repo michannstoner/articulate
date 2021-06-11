d4a067107f2ccc5f837c17eec011a8338ec9df7c
Your Name <stoner.michann@gmail.com>
2021-06-10 19:14:14 -0600

    Add synonyms to display

--- src/components/Word/Word.js
+++ src/components/Word/Word.js
@@ -1,12 +1,18 @@
 import './Word.css' 
 import PropTypes from 'prop-types'
+import pinkSquare from '../../assets/pink-square-icon.png'
 
-const Word = ({ word, pronunciation, definition }) => {
+const Word = ({ word, pronunciation, definition, synonyms }) => {
+  const formattedSynonyms = synonyms.join(', ')
   return (
     <article className='word-card'>
       <h3 className='single-word'>{word}</h3>
-      <p>{pronunciation}</p>
-      <p>{definition}</p>
+      <div className='definition-divider'>
+        <img className='pink-square' src={pinkSquare}/>
+        <p className='definition'>{definition}</p>
+      </div>
+      <p><strong>pronunciation:</strong> {pronunciation}</p>
+      <p><strong>synonyms:</strong> {formattedSynonyms}</p>
     </article>
   )
 }
