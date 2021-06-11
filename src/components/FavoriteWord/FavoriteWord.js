67eb0a5bcf09b8d9509fb6735d7f92a9eb78a6bc
Your Name <stoner.michann@gmail.com>
2021-06-11 12:20:57 -0600

    Add proptypes for favorite word

--- src/components/FavoriteWord/FavoriteWord.js
+++ src/components/FavoriteWord/FavoriteWord.js
@@ -1,5 +1,6 @@
 import './FavoriteWord.css'
 import minusIcon from '../../assets/minus-icon.png'
+import PropTypes from 'prop-types'
 
 const FavoriteWord = ({ word, id, removeFromFavorites }) => {
   return (
@@ -8,6 +9,12 @@ const FavoriteWord = ({ word, id, removeFromFavorites }) => {
       <img id={id} onClick={removeFromFavorites} className='minus-icon' src={minusIcon} alt='minus icon to remove from favorites'/>
     </div>
   )
+}
+
+FavoriteWord.propTypes = {
+  word: PropTypes.string,
+  id: PropTypes.number,
+  removeFromFavorites: PropTypes.func
 }
 
 export default FavoriteWord
