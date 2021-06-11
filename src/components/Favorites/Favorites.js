6adb3ecb3f74a1a611309fbb02c77e04518f7580
Your Name <stoner.michann@gmail.com>
2021-06-11 12:13:06 -0600

    Add remove from favorites

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -2,11 +2,13 @@ import './Favorites.css'
 import FavoriteWord from '../FavoriteWord/FavoriteWord'
 import propTypes from 'prop-types'
 
-const Favorites = ({ favoriteWords }) => {
+const Favorites = ({ favoriteWords, removeFromFavorites }) => {
   const favoritesList = favoriteWords.map(word => {
     return <FavoriteWord 
           key={word.frequency}
           word={word.word}
+          id={word.frequency}
+          removeFromFavorites={removeFromFavorites}
           />
   })
 
