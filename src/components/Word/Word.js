9dbe4b3e779711837e8140bfe805b6e71a8b8d2f
Your Name <stoner.michann@gmail.com>
2021-06-10 18:48:02 -0600

    Give classname to single word

--- src/components/Word/Word.js
+++ src/components/Word/Word.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const Word = ({ word, pronunciation, definition }) => {
   return (
     <article className='word-card'>
-      <h3>{word}</h3>
+      <h3 className='single-word'>{word}</h3>
       <p>{pronunciation}</p>
       <p>{definition}</p>
     </article>
