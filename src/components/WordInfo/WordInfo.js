5ddc32e976584883c52aab7090451c332b0edd2a
Your Name <stoner.michann@gmail.com>
2021-06-10 19:28:04 -0600

    Import and implement heart icon, remove button

--- src/components/WordInfo/WordInfo.js
+++ src/components/WordInfo/WordInfo.js
@@ -2,6 +2,7 @@ import './WordInfo.css'
 import Word from '../Word/Word'
 import PropTypes from 'prop-types'
 import blueBlobIcon from '../../assets/blue-blob-icon.png'
+import heartIcon from '../../assets/heart-icon.png'
 
 const WordInfo = ({ wordToDisplay, addToFavorites }) => {
   const word = 
@@ -16,8 +17,12 @@ const WordInfo = ({ wordToDisplay, addToFavorites }) => {
   return (
     <section className='word-container'>
       <div>
+
       {word}
-      <button onClick={event => addToFavorites(event)}>favorites</button>
+      </div>
+      <div className='favorite-button-container'>
+        <img className='heart-icon' src={heartIcon} alt='heart icon' onClick={event => addToFavorites(event)}/>
+        <p className='add-to-favorites'>add to favorites</p>
       </div>
     </section>
   )
