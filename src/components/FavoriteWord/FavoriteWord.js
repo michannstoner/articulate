e7a49e2107d9c2eff2413643c74bd6fe972aa6e5
Your Name <stoner.michann@gmail.com>
2021-06-11 12:14:22 -0600

    Update alt text

--- src/components/FavoriteWord/FavoriteWord.js
+++ src/components/FavoriteWord/FavoriteWord.js
@@ -5,7 +5,7 @@ const FavoriteWord = ({ word, id, removeFromFavorites }) => {
   return (
     <div className='fav-word-container'>
       <ul>{word}</ul>
-      <img id={id} onClick={removeFromFavorites} className='minus-icon' src={minusIcon} alt='minus icon remove from favorites'/>
+      <img id={id} onClick={removeFromFavorites} className='minus-icon' src={minusIcon} alt='minus icon to remove from favorites'/>
     </div>
   )
 }
