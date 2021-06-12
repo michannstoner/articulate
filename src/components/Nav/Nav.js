0d392032479e28cfecf280f7e521d183830317de
Your Name <stoner.michann@gmail.com>
2021-06-12 12:24:32 -0600

    Import NavLink, add active styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,19 +1,20 @@
 import './Nav.css'
-import { Link } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
 const Nav = () => {
   const linkStyle = {textDecoration: 'none', color: 'black'}
+  const activeStyle = {fontWeight: 700}
 
   return (
     <nav className='nav'>
     <Link to='/' style={linkStyle}>
       <h1 className='page-header'>A.</h1>
     </Link>
-    <Link to={'/favorites'} style={linkStyle}>
+    <NavLink to={'/favorites'} style={linkStyle} activeStyle={activeStyle}>
     <div className='link-divider'>
       <ul className='favorites-link'>favorites</ul>
       </div>
-    </Link>
+    </NavLink>
     </nav>
   )
 }
