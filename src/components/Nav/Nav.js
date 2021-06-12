26a89ec02bcab7a435700d3aa4c2bacf0be09751
Your Name <stoner.michann@gmail.com>
2021-06-12 16:43:43 -0600

    Add method to header

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,18 +1,18 @@
 import './Nav.css'
 import { NavLink, Link } from 'react-router-dom'
 
-const Nav = () => {
+const Nav = ({ resetPage }) => {
   const linkStyle = {textDecoration: 'none', color: 'black'}
   const activeStyle = {fontWeight: 700}
 
   return (
     <nav className='nav'>
     <Link to='/' style={linkStyle}>
-      <h1 className='page-header'>A.</h1>
+      <h1 className='page-header' onClick={resetPage}>A.</h1>
     </Link>
     <NavLink to={'/favorites'} style={linkStyle} activeStyle={activeStyle}>
     <div className='link-divider'>
-      <ul className='favorites-link'>favorites</ul>
+      <p className='favorites-link'>favorites</p>
       </div>
     </NavLink>
     </nav>
