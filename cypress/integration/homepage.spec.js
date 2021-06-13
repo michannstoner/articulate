4dafcafa17725a96f20a7875aac89c385169c5d7
Your Name <stoner.michann@gmail.com>
2021-06-13 08:05:38 -0600

    Add test for word display and search input clear

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -22,4 +22,31 @@ describe('Homepage', () => {
       .get('.welcome-header').should('be.visible').should('contain', 'welcome to ARTICULATE.')
       .get('.search-to-learn').should('be.visible').should('contain', 'look up a word & start learning')
   })
+
+  it('should allow a user to search for a word', () => {
+    cy.get('input[type="text"]')
+      .type('lucky')
+      .should('be.visible').should('have.value', 'lucky')
+      .get('.search-button').should('be.visible')
+      .click()
+  })
+
+  it('should display the search results, if the word was found', () => {
+    cy.get('input[type="text"]')
+      .type('happy')
+      .get('.search-button').click()
+      .get('.blue-blob-icon').should('be.visible')
+      .get('.single-word').should('be.visible').should('contain', 'happy')
+      .get('.definition').should('be.visible').should('contain', 'marked by good fortune')
+      .get('p').should('be.visible').should('contain', 'pronunciation:')
+      .should('contain', 'synonyms: felicitous')
+      .should('contain', 'example: a happy outcome')
+  })
+
+  it('should clear the input after a search has been made', () => {
+    cy.get('input[type="text"]')
+      .type('happy')
+      .get('.search-button').click()
+      .get('input[placeholder="search for a word"]').should('be.visible').should('have.value', '')
+  })
 })
