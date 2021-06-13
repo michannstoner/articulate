4c7ef0a98eb9f6fb432db5d679258a2982e362d5
Your Name <stoner.michann@gmail.com>
2021-06-12 21:23:54 -0600

    Write beginning test for input

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -7,5 +7,13 @@ describe('Homepage', () => {
   it('should have a nav', () => {
     cy.get('nav').should('be.visible')
       .get('h1').should('contain', 'A.').should('be.visible')
+      .get('p').should('contain', 'favorites').should('be.visible')
+  })
+
+  it('should have an input to search for a word', () => {
+    cy.get('form').should('be.visible')
+      .get('input[placeholder="search for a word"]').should('be.visible')
+      .get('input[type="text"')
+      .type('lucky').should('have.value', 'lucky')
   })
 })
