accd6a05e8a0a6952da402753394861782c5ea85
Your Name <stoner.michann@gmail.com>
2021-06-14 11:24:29 -0600

    Refactor to use beforeEach

--- cypress/integration/favorites.spec.js
+++ cypress/integration/favorites.spec.js
@@ -16,58 +16,33 @@ describe('Favorites', () => {
       .url().should('eq', 'http://localhost:3000/')
   })
 
-  it('should display a favorite word if a word has been added', () => {
+describe('Adding Favorites', () => {
+  beforeEach(() => {
     cy.get('.home-icon').click()
       .get('input[type="text"]').type('lucky')
-      .get('.search-button').click()
-      .fixture('test-data.json')
-        .then(testData => {
-          cy.intercept('https://wordsapiv1.p.rapidapi.com/words/lucky', 
-            {
-              "method": "GET",
-              "headers": 
-              {
-                "x-rapidapi-key": "247406a57bmsh73bf0cce7ec0b4cp15cc72jsnf9d8d4aae 1f1",
-                "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
-              }
-            }, 
-            {
-              statusCode: 201,
-              delay: 100,
-              body: testData[0]
-            })
-        })
+      .get('.go-button').click()
+      .intercept('https://wordsapiv1.p.rapidapi.com/words/lucky', 
+      {
+        "method": "GET",
+        "headers": 
+        {
+          "x-rapidapi-key": "247406a57bmsh73bf0cce7ec0b4cp15cc72jsnf9d8d4aae 1f1",
+          "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
+        }
+      }, { fixture: 'test-data.json' })
       .get('.heart-icon').click()
       .get('.favorites-link').click()
-      .get('.favorites-header').should('be.visible').should('contain', 'Favorites')
+  })
+  it('should display a favorite word if a word has been added', () => {
+      cy.get('.favorites-header').should('be.visible').should('contain', 'Favorites')
       .get('.list-word').should('be.visible').should('contain', 'lucky')
       .get('.trash-icon').should('be.visible')
   })
-
+  
   it('should remove a word if the trash can icon is clicked', () => {
-    cy.get('.home-icon').click()
-    .get('input[type="text"]').type('lucky')
-    .get('.search-button').click()
-    .fixture('test-data.json')
-      .then(testData => {
-        cy.intercept('https://wordsapiv1.p.rapidapi.com/words/lucky', 
-          {
-            "method": "GET",
-            "headers": 
-            {
-              "x-rapidapi-key": "247406a57bmsh73bf0cce7ec0b4cp15cc72jsnf9d8d4aae1f1",
-              "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
-            }
-          }, 
-          {
-            statusCode: 201,
-            delay: 100,
-            body: testData[0]
-          })
-      })
-    .get('.heart-icon').click()
-    .get('.favorites-link').click()
-    .get('.trash-icon').click()
+    cy.get('.trash-icon').click()
     .get('.no-favorites').should('be.visible').should('contain', 'Nothing in favorites yet!')
   })
+})
+
 })
